06b1a50db9d30c77bf7a5283b3ab5b08a628c457
Ericka Zavala <contacto@ezavalar.com>
2025-09-01 17:50:08 -0600

    Se agregó main.py con dos rutas GET y activación de CORS

---
+++ server-flask/main.py
@@ -0,0 +1,24 @@
+from flask import Flask
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)  # Habilitar CORS para todas las rutas, se recomienda especificar orígenes en producción, solo a donde se permite el acceso
+
+@app.route('/') 
+def holaMundo():
+    return '¡Hola Mundo!'
+
+@app.route('/api/users')
+def obtenerUsuarios():
+    return {
+        'users': ['Alice', 'Bob', 'Charlie']
+    }
+
+@app.route('/api/frutas')
+def obtenerFrutas():
+    return {
+        'frutas': ['Manzana', 'Banana', 'Cereza']
+    }
+
+if __name__ == '__main__':
+    app.run(debug=True) #Por defecto corre en el puerto 5000
